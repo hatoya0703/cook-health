3a78e9dc2cda894c31cf2e3037df54980bed26d1
hatoya0703 <hatoya0703@gmail.com>
2021-01-19 19:14:33 +0900

    update:userテーブルマイグレーションファイル編集

--- db/migrate/20210119093124_devise_create_users.rb
+++ db/migrate/20210119093124_devise_create_users.rb
@@ -4,8 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false, default: "", comment: "ユーザー名"
+      t.string :email,              null: false, default: "", comment: "登録メールアドレス"
+      t.string :encrypted_password, null: false, default: "", comment: "ログインパスワード"
 
       ## Recoverable
       t.string   :reset_password_token
