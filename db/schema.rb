2fed4c54eac6d7d448e11e48dece2be2f61b3eb0
hatoya0703 <hatoya0703@gmail.com>
2021-01-19 19:51:35 +0900

    add:ingredientsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_102609) do
+ActiveRecord::Schema.define(version: 2021_01_19_103410) do
+
+  create_table "ingredients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "material", null: false, comment: "材料名"
+    t.string "quantity", null: false, comment: "分量"
+    t.bigint "recipe_id", null: false, comment: "対応レシピ"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
 
   create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false, comment: "レシピ名"
@@ -34,5 +43,6 @@ ActiveRecord::Schema.define(version: 2021_01_19_102609) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ingredients", "recipes"
   add_foreign_key "recipes", "users"
 end
