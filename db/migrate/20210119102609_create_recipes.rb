3703691b7baa7b9d15366b0f840fed99352e47b4
hatoya0703 <hatoya0703@gmail.com>
2021-01-19 19:32:59 +0900

    update:recipeテーブルマイグレーションファイル修正

--- db/migrate/20210119102609_create_recipes.rb
+++ db/migrate/20210119102609_create_recipes.rb
@@ -1,7 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
-    create_table :recipes do |t|
 
+    create_table :recipes do |t|
+      t.string     :title,   null: false, comment: "レシピ名"
+      t.text       :content, null: false, comment: "レシピ説明"
+      t.references :user,    null: false, foreign_key: true, comment: "投稿ユーザー"
       t.timestamps
     end
   end
