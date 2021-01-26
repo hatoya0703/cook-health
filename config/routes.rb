4a4dd944fd74f737d7cc26f2ca445dce9cbbace4
hatoya0703 <hatoya0703@gmail.com>
2021-01-26 21:40:37 +0900

    update:ヘッダーにログイン関係ボタン及びリンク付

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'recipes#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :rercipes, only: :index
+  resources :recipes, only: :index
 end
