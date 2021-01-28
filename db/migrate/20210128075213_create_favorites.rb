f8f2c56a9847e28a2f870afea46eed94b0402c6f
hatoya0703 <hatoya0703@gmail.com>
2021-01-28 18:18:15 +0900

    update:favoritesテーブルマイグレーションファイル修正

--- db/migrate/20210128075213_create_favorites.rb
+++ db/migrate/20210128075213_create_favorites.rb
@@ -1,7 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[6.0]
   def change
     create_table :favorites do |t|
-
+      t.references user,   null: false, foreign_key: true, comment: "お気に入りしたユーザー"
+      t.references recipe, null: false, foreign_key: true, comment: "お気に入りしたレシピ"
       t.timestamps
     end
   end
