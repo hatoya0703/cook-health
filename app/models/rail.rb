08420c238202e82cf05c8caea4ecc124c629d91d
hatoya0703 <hatoya0703@gmail.com>
2021-01-28 17:35:57 +0900

    remove:不要モデルrail削除

--- app/models/rail.rb
+++
@@ -1,2 +0,0 @@
-class Rail < ApplicationRecord
-end
--- spec/factories/rails.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :rail do
-    g { "MyString" }
-    model { "MyString" }
-    RecipeTags { "MyString" }
-  end
-end
