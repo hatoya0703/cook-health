8430e6388ec6c9fe7e37ae527c3dd5f2abb90821
hatoya0703 <hatoya0703@gmail.com>
2021-01-28 18:27:55 +0900

    update:food_nutrientsテーブルマイグレーションファイル修正

--- db/migrate/20210128075828_create_food_nutrients.rb
+++ db/migrate/20210128075828_create_food_nutrients.rb
@@ -1,7 +1,8 @@
 class CreateFoodNutrients < ActiveRecord::Migration[6.0]
   def change
     create_table :food_nutrients do |t|
-
+      t.references food,     null:false, foreign_key: true, comment: "対応レシピ"
+      t.references nutrient, null:false, foreign_key: true, comment: "対応栄養"
       t.timestamps
     end
   end
