7fe4f41677f809125c252241eed0be0f775d09ea
hatoya0703 <hatoya0703@gmail.com>
2021-01-28 18:15:27 +0900

    update:commentsテーブルマイグレーションファイル修正

--- db/migrate/20210128075152_create_comments.rb
+++ db/migrate/20210128075152_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-
+      t.text       comment_content, null: false,                    comment: "コメント内容"
+      t.references user,            null: false, foreign_key: true, comment: "投稿ユーザー"
+      t.references recipes,         null: false, foreign_key: true, comment: "投稿レシピ"
       t.timestamps
     end
   end
