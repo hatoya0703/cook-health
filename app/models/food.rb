fb7abb09692b2d37364bb35435a299cd9f4c9202
hatoya0703 <hatoya0703@gmail.com>
2021-01-28 17:16:26 +0900

    update:foodモデルアソシエーション記述

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,7 @@
 class Food < ApplicationRecord
+  # アソシエーション
+  has_many :food_recipes
+  has_many :recipes through: :food_recipes
+  has_many :food_nutrients
+  has_many :nutrients through: :food_nutrients
 end
