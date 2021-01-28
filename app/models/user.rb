7fe4293bd86fbc9e76f9844c2a2a9b533a51c853
hatoya0703 <hatoya0703@gmail.com>
2021-01-28 17:05:41 +0900

    update:uesrモデルアソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # アソシエーション
   has_many :recipes
+  has_many :comments
+  has_many :comment_recipes, through: :comments, source: :recipe
+  has_many :favorites
+  has_many :favorite_recipes, through: :favorites, source: :recipe
 end
