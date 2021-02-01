e90350898a80216d122cee1f0a96bde0c6bc6d68
hatoya0703 <hatoya0703@gmail.com>
2021-02-01 14:11:22 +0900

    update:categoryモデルのデータ格納

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,15 @@
 class Category < ActiveHash::Base
+  self.date = [
+    { id: 1, name: "--" },
+    { id: 2, name: "ごはんもの" },
+    { id: 3, name: "お肉のおかず" },
+    { id: 4, name: "魚介のおかず" },
+    { id: 5, name: "野菜のおかず" },
+    { id: 6, name: "パスタ・グラタン" },
+    { id: 7, name: "サラダ" },
+    { id: 8, name: "麺" },
+    { id: 9, name: "スープ・汁物" },
+    { id: 10, name: "パン" },
+    { id: 11, name: "その他" }
+  ]
 end
