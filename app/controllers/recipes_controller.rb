360ea14bd12e25c5b7bbb1ece22dff93dcb99480
hatoya0703 <hatoya0703@gmail.com>
2021-02-01 15:01:40 +0900

    update:recipes#new投稿フォーム基礎設置

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,7 +9,13 @@ class RecipesController < ApplicationController
   end
 
   def create
-    
+    @recipe = Recipe.new(recipe_params)
+    @recipe.save
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :content, :category_id).merge(user_id: current_user.id)
   end
 
 end
