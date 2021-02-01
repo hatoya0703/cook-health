6ddb1fa7d3eadb11245e1f9088fefeba5ca74125
hatoya0703 <hatoya0703@gmail.com>
2021-02-01 09:59:22 +0900

    update:trubolinksの一部無効化により、Ajax動作するように修正

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,6 +1,7 @@
+$(document).on('turbolinks:load', function(){
 $(window).on("load", function(){
   $(".btn").click(function(event){
     event.preventDefault();
       $(".nav").stop().slideToggle();
     });
-});
+})});
