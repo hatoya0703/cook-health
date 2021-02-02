9dcf4df2857e5d58ee4a2971eb2f8e184cd3338d
hatoya0703 <hatoya0703@gmail.com>
2021-02-02 17:55:40 +0900

    update:recipes#newのformのinput追加

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,4 @@
 class RecipeIngredient
   include ActiveModel::Model
-  attr_accesser :title, :content, :category_id, :material, :quantity
+  attr_accessor :title, :content, :category_id, :material, :quantity
 end
