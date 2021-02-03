c1d0c62ef818231e869ed2944c2e1e016d2c24b2
hatoya0703 <hatoya0703@gmail.com>
2021-02-03 19:34:05 +0900

    update:formの画像保存対応

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,12 +11,13 @@ class RecipesController < ApplicationController
   def create
     @recipe = RecipeIngredient.new(recipe_params)
     @recipe.save
+    binding.pry
     redirect_to user_path(current_user.id)
   end
 
   private
   def recipe_params
-    params.require(:recipe_ingredient).permit(:title, :content, :category_id, :description, :material, :quantity).merge(user_id: current_user.id)
+    params.require(:recipe_ingredient).permit(:title, :content, :category_id, :description, :material, :quantity, images:[]).merge(user_id: current_user.id)
   end
 
 end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,9 +1,9 @@
 class RecipeIngredient
   include ActiveModel::Model
-  attr_accessor :title, :content, :category_id, :description, :material, :quantity, :user_id
+  attr_accessor :title, :content, :category_id, :description, :material, :quantity, :user_id, :images
   
   def save
-    recipe = Recipe.create(title: title, category_id: category_id, description: description, content: content,user_id: user_id)
+    recipe = Recipe.create(title: title, category_id: category_id, images: images,description: description, content: content,user_id: user_id)
     Ingredient.create(material: material, quantity: quantity, recipe_id: recipe.id)
   end
 
