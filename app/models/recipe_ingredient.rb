877081d6468fc116213dbd793632dbe410990b46
hatoya0703 <hatoya0703@gmail.com>
2021-02-04 14:04:59 +0900

    update:formから紐付けるnutrient.id保存

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,9 +1,9 @@
 class RecipeIngredient
   include ActiveModel::Model
-  attr_accessor :title, :content, :category_id, :description, :material, :quantity, :user_id, :images
+  attr_accessor :title, :content, :category_id, :description, :material, :quantity, :user_id, :images, :nutrient_ids
   
   def save
-    recipe = Recipe.create(title: title, category_id: category_id, images: images,description: description, content: content,user_id: user_id)
+    recipe = Recipe.create(title: title, category_id: category_id, images: images,description: description, content: content,user_id: user_id, nutrient_ids: nutrient_ids )
     Ingredient.create(material: material, quantity: quantity, recipe_id: recipe.id)
   end
 
