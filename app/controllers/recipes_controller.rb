ac71e1b5e67f6c84ef068237cbab51bb4d3cc90e
hatoya0703 <hatoya0703@gmail.com>
2021-02-05 17:15:37 +0900

    update:タグ登録複数対応

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,13 +10,17 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = RecipeIngredient.new(recipe_params)
+    tag_list = params[:recipe_ingredient][:tag_name].split(',')
+    binding.pry
+    @recipe.find_or_create_tags(tag_list)
     @recipe.save
     redirect_to user_path(current_user.id)
   end
 
   private
   def recipe_params
-    params.require(:recipe_ingredient).permit(:title, :content, :category_id, :description, :material, :quantity, images:[], nutrient_ids:[]).merge(user_id: current_user.id)
+    params.require(:recipe_ingredient).permit(:title, :content, :category_id, :description, :material, :quantity, :tag_name, tag_ids:[], images:[], nutrient_ids:[]).merge(user_id: current_user.id, tag_ids:[])
   end
+
 
 end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,10 +1,16 @@
 class RecipeIngredient
   include ActiveModel::Model
-  attr_accessor :title, :content, :category_id, :description, :material, :quantity, :user_id, :images, :nutrient_ids
+  attr_accessor :title, :content, :category_id, :description, :material, :quantity, :user_id, :images, :nutrient_ids, :tag_ids, :tag_name
   
   def save
-    recipe = Recipe.create(title: title, category_id: category_id, images: images,description: description, content: content,user_id: user_id, nutrient_ids: nutrient_ids )
+    recipe = Recipe.create(title: title, category_id: category_id, images: images,description: description, content: content,user_id: user_id, nutrient_ids: nutrient_ids, tag_ids: tag_ids)
     Ingredient.create(material: material, quantity: quantity, recipe_id: recipe.id)
   end
 
+  def find_or_create_tags(post_tags)
+    post_tags.each do |tag_name|
+      post_recipe_tag = Tag.find_or_create_by(tag_name: tag_name)
+      self.tag_ids << post_recipe_tag.id
+    end
+  end
 end
