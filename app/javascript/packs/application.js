380b38c0db020eeca4bb932eb292a6944631ea10
hatoya0703 <hatoya0703@gmail.com>
2021-02-07 15:45:37 +0900

    add:JQuert-UI導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require("jquery-ui/ui/widgets/autocomplete");
 require("../tab")
 require("../menu")
 require("../preview")
