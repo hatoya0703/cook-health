5bf378e5c6e6f7e3c7bd1d0dd69284560e6ef65a
hatoya0703 <hatoya0703@gmail.com>
2021-02-11 17:41:31 +0900

    update:ヘッダーとフッターをaplication.html.erbへ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
+
+  def set_search
+    @search = Recipe.ransack(params[:q])
+    @search_recipes = @search.result
+  end
+  
 end
