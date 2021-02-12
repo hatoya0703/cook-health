f517a9975ca612a07112b4b3e9d14279eb5c5963
hatoya0703 <hatoya0703@gmail.com>
2021-02-12 16:22:09 +0900

    update:各モデルにdependent: :destroy追記

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,14 +1,14 @@
 class Recipe < ApplicationRecord
   # アソシエーション
   belongs_to :user
-  has_many :ingredients
-  has_many :comments
+  has_many :ingredients, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :users, through: :comments
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
-  has_many :recipe_tags
+  has_many :recipe_tags, dependent: :destroy
   has_many :tags, through: :recipe_tags
-  has_many :recipe_nutrients
+  has_many :recipe_nutrients, dependent: :destroy
   has_many :nutrients, through: :recipe_nutrients
 
   # ActiveHashのアソシエーション
@@ -16,5 +16,5 @@ class Recipe < ApplicationRecord
   belongs_to_active_hash :category
 
   # ActiveStorageのアソシエーション
-  has_many_attached :images
+  has_many_attached :images, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # アソシエーション
-  has_many :recipes
-  has_many :comments
+  has_many :recipes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :comment_recipes, through: :comments, source: :recipe
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :favorite_recipes, through: :favorites, source: :recipe
 end
