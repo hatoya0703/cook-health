fa5fd25df7980eef0cf25dc0438e1b9f889b6ec5
hatoya0703 <hatoya0703@gmail.com>
2021-02-12 15:43:02 +0900

    update:ransackのデフォルトソート明記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   def set_search
     @search = Recipe.ransack(params[:q])
+    @search.sorts = 'updated_at desc' if @search.sorts.empty?
     @search_recipes = @search.result.page(params[:page]).per(10).includes(:user).with_attached_images
   end
 end
