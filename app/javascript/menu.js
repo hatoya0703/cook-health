6fcb25ce99a318e8b0d155d64b7adbaca7c8d435
hatoya0703 <hatoya0703@gmail.com>
2021-02-15 15:55:58 +0900

    update:menu.jsコメント追記

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,6 +1,7 @@
 $(document).on('turbolinks:load', function(){
-  $(".btn").on("click",function(event){
-    event.preventDefault();
+  $(".btn").on("click",function(e){
+    e.preventDefault();
+      // display:none;が設定してあるnavクラスを、slideToggleで表示させる
       $(".nav").stop().slideToggle();
     });
 });
