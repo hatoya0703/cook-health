2e2aa69ad47380a91d93243bc5a66d2289dc3146
hatoya0703 <hatoya0703@gmail.com>
2021-02-23 16:47:25 +0900

    add:congit/locales/devise/views/ja/yaml作成

--- Gemfile
+++ Gemfile
@@ -66,6 +66,8 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
+gem 'devise-i18n'
+gem 'devise-i18n-views'
 gem 'pry-rails'
 gem 'rails-i18n'
 gem 'jquery-rails'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :set_search
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_search
     @search = Recipe.ransack(params[:q])
     @search.sorts = 'updated_at desc' if @search.sorts.empty?
     @search_recipes = @search.result.page(params[:page]).per(1).includes(:user).with_attached_images
+  end
+
+  def configure_permitted_parameters
+    add_columns = [:nickname]
+    devise_parameter_sanitizer.permit(:sign_up, keys: add_columns)
   end
 end
