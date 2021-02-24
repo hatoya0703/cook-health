26a3a61b3e28e9b8490ee5838e5d6b8bde373883
hatoya0703 <hatoya0703@gmail.com>
2021-02-24 18:47:39 +0900

    update:userテストモデル作成及び、パスワードバリデーション最高文字数追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.email }
+    password              { Faker::Internet.password(min_length: 6, max_length: 20, mix_case: true)}
+    password_confirmation { password }
   end
 end
