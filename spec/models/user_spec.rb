fde7f4f1b8be11f0aedcf005ccfd06beb53494a5
hatoya0703 <hatoya0703@gmail.com>
2021-02-24 18:53:46 +0900

    update:userモデル単体テストの正常系作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    context '正常に登録できる時' do
+      it 'nickname,email,passwordを入力すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+    
+  end
 end
