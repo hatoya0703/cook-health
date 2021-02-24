c9d85751a1083fb4c4b9b5c473e018382f30494c
hatoya0703 <hatoya0703@gmail.com>
2021-02-24 19:02:27 +0900

    update:userモデル単体テストの異常系項目作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,53 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
     end
-    
+
+    context '登録できない時' do
+      it 'nicknameが空では登録できない' do
+        
+      end
+      it 'emailが空では登録できない' do
+        
+      end
+      it 'emailに@が含まれていないと登録できない' do
+        
+      end
+      it 'emailにひらがなが含まれていると登録できない' do
+        
+      end
+      it 'emailに漢字が含まれていると登録できない' do
+        
+      end
+      it '既に使用されているemailでは登録できない' do
+        
+      end
+      it 'passwordが空では登録できない' do
+        
+      end
+      it 'password_confirmationが空では登録できない' do
+        
+      end
+      it 'passwordが5文字以下では登録できない' do
+        
+      end
+      it 'passwordが21文字以上では登録できない' do
+        
+      end
+      it 'passwordが英語のみでは登録できない' do
+
+      end
+      it 'passwordが数字だけでは登録できない' do
+        
+      end
+      it 'passwordにひらがなが含まれていると登録できない' do
+
+      end
+      it 'passwordに漢字が含まれていると登録できない' do
+        
+      end
+      it 'passwordとpassword_confirmationが一致していなければ登録できない' do
+        
+      end
+    end
   end
 end
