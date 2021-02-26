b69e847678780cf71030bc2bcc4e472e96d715ed
hatoya0703 <hatoya0703@gmail.com>
2021-02-26 17:20:51 +0900

    update:nutrientモデルにactivestorageのアソシエーション追記

--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -2,4 +2,6 @@ class Nutrient < ApplicationRecord
   # アソシエーション
   has_many :recipe_nutrients, dependent: :destroy
   has_many :recipes, through: :recipe_nutrients
+
+  has_many_attached :image, dependent: :destroy
 end
