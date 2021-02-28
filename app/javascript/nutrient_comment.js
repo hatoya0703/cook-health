89fdb43f9f619c0b501f99e8adb703744cab56f9
hatoya0703 <hatoya0703@gmail.com>
2021-02-28 15:34:48 +0900

    update:コメント表示位置をイベント発火時の座標を元に指定するように

--- app/javascript/nutrient_comment.js
+++ app/javascript/nutrient_comment.js
@@ -2,14 +2,30 @@ const { getJSON } = require("jquery");
 
 $(document).on("turbolinks:load",function(){
   $("label").hover(
-    function(){
+    function(e){
     //ホバーした栄養のcheckboxのvalueの値を変数numに格納
     const num = this.firstChild.value
     //gon.nutrient_allにはコントローラーの処理でNutrient.allが格納されている。
     //配列gon.nutrient_allの[value - 1]番目のコメントを取得
     const nut_com = gon.nutrient_all[num - 1].nutrient_comment
-    
+    //コメント挿入するためのpタグを生成
+    $(this).append("<span>");
+    $(this.lastChild).addClass("arrow_box");
+    //pタグにコメントテキスト挿入
+    $(this.lastChild).text(nut_com);
+
+    //吹き出しを表示させる座標を決めるため、イベント発火時の座標を取得
+    const mouseX = e.clientX
+    const mouseY = e.clientY
+    //吹き出しのCSSを上書きして、表示座標を指定
+    $("span.arrow_box").css({
+      "position": "absolute",
+      "left": mouseX,
+      "top": mouseY
+    });
+
   },function(){
-    console.log("ホバーハズレテスト")
+    //挿入したpタグコメントを削除
+    $(this.lastChild).remove("span")
   });
 })
