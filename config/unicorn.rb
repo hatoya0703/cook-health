75ed0d449f82419fca1dd268887348667b2b3369
hatoya0703 <hatoya0703@gmail.com>
2021-03-02 14:27:42 +0900

    update:path変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,14 +1,14 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory "#{app_path}/current"
+working_directory "#{app_path}/cook-health/current"
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+pid "#{app_path}/cook-health/shared/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
