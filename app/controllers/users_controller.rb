d22d4a23cd7bca3e3611ed2f56e861f4df82b7d8
hatoya0703 <hatoya0703@gmail.com>
2021-03-06 19:59:22 +0900

    update:マイページにお気に入りしたレシピをページネーションで表示するように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,9 @@ class UsersController < ApplicationController
   def show
     if user_signed_in?
       favorites = Favorite.where(user_id: current_user.id).order(created_at: :DESC).pluck(:recipe_id)
-      @favorite_recipes = Recipe.find(favorites)
+      @favorite_recipes_array = Recipe.includes(:user).with_attached_images.find(favorites)
+      # @favorite_recipes_arrayがArrayになっており、pageメソッドが使用できないため
+      @favorite_recipes = Kaminari.paginate_array(@favorite_recipes_array).page(params[:page]).per(1)
     end
   end
-
 end
