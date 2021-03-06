cb7fb91e94ff55ee9a618ed073bfb981f14a9c84
hatoya0703 <hatoya0703@gmail.com>
2021-03-06 20:25:53 +0900

    update:マイページ表示条件分岐(ログインユーザー=マイページユーザー)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,16 @@ class UsersController < ApplicationController
 
   def show
     if user_signed_in?
-      favorites = Favorite.where(user_id: current_user.id).order(created_at: :DESC).pluck(:recipe_id)
-      @favorite_recipes_array = Recipe.includes(:user).with_attached_images.find(favorites)
-      # @favorite_recipes_arrayがArrayになっており、pageメソッドが使用できないため
-      @favorite_recipes = Kaminari.paginate_array(@favorite_recipes_array).page(params[:page]).per(1)
+      if current_user.id == params[:id].to_i
+        # マイレシピ表示用
+        @my_recipes = Recipe.where(user_id: current_user.id).order(created_at: :DESC).includes(:user).with_attached_images.page(params[:page]).per(1)
+
+        # お気に入りレシピ表示用
+        favorites = Favorite.where(user_id: current_user.id).order(created_at: :DESC).pluck(:recipe_id)
+        @favorite_recipes_array = Recipe.includes(:user).with_attached_images.find(favorites)
+        # @favorite_recipes_arrayがArrayになっており、pageメソッドが使用できないため
+        @favorite_recipes = Kaminari.paginate_array(@favorite_recipes_array).page(params[:page]).per(1)
+      end
     end
   end
 end
