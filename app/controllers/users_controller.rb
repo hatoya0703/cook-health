c06688600c6e1eb3f03845de7bdcea6e80e342bb
hatoya0703 <hatoya0703@gmail.com>
2021-03-06 18:50:44 +0900

    update:indexのビュー調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  # if user_signed_in?
-    # favorites = Favorite.where(user_id: current_user.id).order(created_at: :DESC).pluck(:recipe_id)
-    # @favorite_recipes = Recipe.find(favorites)
-  # end
+
+  def show
+    if user_signed_in?
+      favorites = Favorite.where(user_id: current_user.id).order(created_at: :DESC).pluck(:recipe_id)
+      @favorite_recipes = Recipe.find(favorites)
+    end
+  end
+
 end
