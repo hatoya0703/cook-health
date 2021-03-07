5b214a792176869323bc31ab690e661393fb3288
hatoya0703 <hatoya0703@gmail.com>
2021-03-07 18:19:57 +0900

    update:タブ切り替えボタン設置（デザインは暫定）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,10 @@ class UsersController < ApplicationController
   def show
     if user_signed_in?
       if current_user.id == params[:id].to_i
+        # マイレシピ数カウント
+        @my_recipe_count = Recipe.where(user_id: current_user.id).count
         # マイレシピ表示用
         @my_recipes = Recipe.where(user_id: current_user.id).order(created_at: :DESC).includes(:user).with_attached_images.page(params[:page]).per(1)
-
         # お気に入りレシピ表示用
         favorites = Favorite.where(user_id: current_user.id).order(created_at: :DESC).pluck(:recipe_id)
         @favorite_recipes_array = Recipe.includes(:user).with_attached_images.find(favorites)
